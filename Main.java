413785628d4d368545e0db34fee36add2f54ff63
Kamran Salihov <donyepoloss@gmail.com>
2025-12-05 20:53:51 +0400

    1.0

--- Main.java
+++ Main.java
@@ -10,15 +10,24 @@ public class Main {
     }
 
     private static void testArrayFunction() throws InappropriateFunctionPointException {
-        TabulatedFunction arrayFunction = new ArrayTabulatedFunction(-1.0, 1.0, 5);
+        double[] yValues = {1.0, 0.5, 0.0, -0.5, -1.0};
+        TabulatedFunction arrayFunction = new ArrayTabulatedFunction(-1.0, 1.0, yValues);
 
         System.out.println("начальные точки:");
         printFunction(arrayFunction);
 
+        System.out.println("\nпроверка getFunctionValue:");
+        System.out.println("f(-1.0) = " + arrayFunction.getFunctionValue(-1.0));
+        System.out.println("f(-0.5) = " + arrayFunction.getFunctionValue(-0.5));
+        System.out.println("f(0.0) = " + arrayFunction.getFunctionValue(0.0));
+        System.out.println("f(0.3) = " + arrayFunction.getFunctionValue(0.3));
+        System.out.println("f(1.0) = " + arrayFunction.getFunctionValue(1.0));
+        System.out.println("f(2.0) (вне диапазона) = " + arrayFunction.getFunctionValue(2.0));
+
         FunctionPoint newPoint = new FunctionPoint(0.57, 0.22);
         arrayFunction.setPoint(3, newPoint);
 
-        System.out.println("после замены точки:");
+        System.out.println("\nпосле замены точки:");
         printFunction(arrayFunction);
 
         FunctionPoint addedPoint = new FunctionPoint(0.71, 0.34);
@@ -44,22 +53,31 @@ public class Main {
             System.out.println("поймано InappropriateFunctionPointException при добавлении существующей точки (0.71, 0.34)");
         }
 
-        arrayFunction.deletePoint(3);
+        arrayFunction.deletePoint(0);
 
-        System.out.println("\nпосле удаления точки:");
+        System.out.println("\nпосле удаления точки с индексом 0:");
         printFunction(arrayFunction);
     }
 
     private static void testLinkedListFunction() throws InappropriateFunctionPointException {
-        TabulatedFunction listFunction = new LinkedListTabulatedFunction(-1.0, 1.0, 5);
+        double[] yValues = {2.0, 1.0, 0.0, -1.0, -2.0};
+        TabulatedFunction listFunction = new LinkedListTabulatedFunction(-1.0, 1.0, yValues);
 
         System.out.println("начальные точки:");
         printFunction(listFunction);
 
+        System.out.println("\nпроверка getFunctionValue:");
+        System.out.println("f(-1.0) = " + listFunction.getFunctionValue(-1.0));
+        System.out.println("f(-0.5) = " + listFunction.getFunctionValue(-0.5));
+        System.out.println("f(0.0) = " + listFunction.getFunctionValue(0.0));
+        System.out.println("f(0.3) = " + listFunction.getFunctionValue(0.3));
+        System.out.println("f(1.0) = " + listFunction.getFunctionValue(1.0));
+        System.out.println("f(2.0) (вне диапазона) = " + listFunction.getFunctionValue(2.0));
+
         FunctionPoint newPoint = new FunctionPoint(0.57, 0.22);
         listFunction.setPoint(3, newPoint);
 
-        System.out.println("после замены точки:");
+        System.out.println("\nпосле замены точки:");
         printFunction(listFunction);
 
         FunctionPoint addedPoint = new FunctionPoint(0.71, 0.34);
@@ -85,11 +103,10 @@ public class Main {
             System.out.println("поймано InappropriateFunctionPointException при добавлении существующей точки (0.71, 0.34)");
         }
 
-        listFunction.deletePoint(3);
+        listFunction.deletePoint(0);
 
-        System.out.println("\nпосле удаления точки:");
+        System.out.println("\nпосле удаления точки с индексом 0:");
         printFunction(listFunction);
-
     }
 
     private static void printFunction(TabulatedFunction func) {
